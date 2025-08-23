f597939e371d8224249160f6855c734bbfae71af
Bijoy Raju <91817954+BijoyRaju@users.noreply.github.com>
2025-08-23 17:19:58 +0530

    Update auth_controller.dart

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
 class AuthController {
   final AuthService _authService = AuthService();
 
